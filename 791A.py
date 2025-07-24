1706d948fc4e0c1baad19dad1402864486df1ca7
Tobias Heyer <saibot133@web.de>
2025-07-24 18:56:47 +0200

    791A: solved

--- 791A.py
+++ 791A.py
@@ -1,22 +1,28 @@
 """
+https://codeforces.com/problemset/problem/791/A
+
 Limak = a *3 per year
 Bob = b *2 per year
 
 Limak < bob == true at start
 """
+import re
+
 
 def get_inputs() -> list[int]:
-    # TODO get inputs from console
-    return [4,9]
+    weights = input()
+    pattern = r"\d+"
+
+    return list(map(int, re.findall(pattern, weights)[:2]))
 
 def compute_years(weight_limak:int, weight_bob:int) -> int:
-    if a>b: return -1
+    if weight_limak>weight_bob: return -1
 
     counter = 0
     limak = weight_limak
     bob = weight_bob
 
-    while limak<=bob:
+    while limak <= bob:
         counter += 1
         limak = limak*3
         bob = bob*2
@@ -25,7 +31,5 @@ def compute_years(weight_limak:int, weight_bob:int) -> int:
 
 if __name__ == '__main__':
     a,b = get_inputs()
-    print(a)
-    print(b)
     years = compute_years(a,b)
     print(years)
